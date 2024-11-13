e8b3080f7995574e374aa1e858e4153182f12c74
Noe Landaverde <noeabraham.landaverde@capitalone.com>
2024-11-13 16:38:25 -0600

    add serial type data size getter

--- app/record.go
+++ app/record.go
@@ -4,13 +4,28 @@ var serialTypes = map[int][2]int{
 	2: [2]int{1, 1},
 }
 
-func SerialTypeData(serialType uint64) (string, uint64) {
-	switch serialType {
+func SerialTypeData(serialInt uint64) (uint64, bool) {
+	switch serialInt {
 	case 0:
-		return "nil", 0
+		return 0, true
 	case 1, 2, 3, 4:
-		return "int", serialType
+		return serialInt, true
+	case 5:
+		return 6, true
+	case 6, 7:
+		return 8, true
+	case 8:
+		return 0, true
+	case 9:
+		return 1, true
+	case 10, 11:
+		return 0, false
 	default:
-		return "", 0
+		if serialInt >= 12 && serialInt%2 == 0 {
+			return (serialInt - 12) / 2, true
+		}
+		if serialInt >= 13 && serialInt%2 != 0 {
+			return (serialInt - 13) / 2, true
+		}
 	}
 }
